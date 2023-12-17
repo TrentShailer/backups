3df4c1a9eb4f1deb2896e02c0799cc4b9b21e29f
Trent Shailer <trent.shailer@gmail.com>
2023-12-18 11:57:01 +1300

    did a payload size check

--- server/src/socket/payload.rs
+++ server/src/socket/payload.rs
@@ -19,15 +19,21 @@ pub struct Payload {
     pub file: Vec<u8>,
 }
 
+const BUFFER_SIZE: usize = 10000000; // 10Mb
+
 pub async fn get_payload(
     reader: &mut ReadHalf<&mut TlsStream<TcpStream>>,
 ) -> Result<Payload, GetPayloadError> {
-    let mut contents: Vec<u8> = vec![0; 10000000]; // 10Mb
+    let mut contents: Vec<u8> = vec![0; BUFFER_SIZE];
 
     let buffer_size = reader
         .read(&mut contents)
         .await
         .map_err(GetPayloadError::ReadStreamError)?;
+
+    if buffer_size == BUFFER_SIZE {
+        return Err(GetPayloadError::PayloadSizeError);
+    }
 
     let contents_string = str::from_utf8(&contents[0..buffer_size])?;
 
@@ -44,4 +50,6 @@ pub enum GetPayloadError {
     ConvertToStringError(#[from] Utf8Error),
     #[error("DeserialzeError[br]{0}")]
     DeserializeError(#[from] toml::de::Error),
+    #[error("PayloadSizeError")]
+    PayloadSizeError,
 }
