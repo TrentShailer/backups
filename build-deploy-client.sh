7b85daf37ad786f4bbb27867478f17226768bf59
Trent Shailer <trent.shailer@gmail.com>
2024-10-01 14:17:48 +1300

    modify backups client to allow user rwx

--- build-deploy-client.sh
+++ build-deploy-client.sh
@@ -1,6 +1,6 @@
 cargo build --locked --release --bin backups-client
 mkdir -p ./live/client/
-chmod a+x ./target/release/backups-client
+chmod u+rwx ./target/release/backups-client
 mv ./target/release/backups-client ./live/client/backups-client
 cd ./live/client
 ./backups-client &
