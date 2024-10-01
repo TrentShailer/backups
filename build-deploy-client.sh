351170d13c5c6da668e60575d57e77838e04ab79
Trent Shailer <trent.shailer@gmail.com>
2024-10-01 14:10:30 +1300

    modify permissions given to backups client exec

--- build-deploy-client.sh
+++ build-deploy-client.sh
@@ -1,6 +1,6 @@
 cargo build --locked --release --bin backups-client
 mkdir -p ./live/client/
-chmod 777 ./target/release/backups-client
+chmod a+x ./target/release/backups-client
 mv ./target/release/backups-client ./live/client/backups-client
 cd ./live/client
 ./backups-client &
