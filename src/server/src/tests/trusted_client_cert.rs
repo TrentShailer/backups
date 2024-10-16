a9dd992cf7c3b1982d0866f5db3ff50d3f8c848a
Trent Shailer <trent.shailer@gmail.com>
2024-10-17 10:27:40 +1300

    cleanup after trusted_client_cert test

--- src/server/src/tests/trusted_client_cert.rs
+++ src/server/src/tests/trusted_client_cert.rs
@@ -1,11 +1,24 @@
-use std::{net::SocketAddr, thread};
+use std::{fs, net::SocketAddr, path::Path, thread};
 
 use crate::server::Server;
 
 use super::{client::TestClient, TestPki};
 
+struct CleanupBackup;
+
+impl Drop for CleanupBackup {
+    fn drop(&mut self) {
+        if Path::new("backups/backups_server/tests/trusted_client_cert").exists() {
+            fs::remove_file("backups/backups_server/tests/trusted_client_cert")
+                .expect("Failed to remove file");
+        }
+    }
+}
+
 #[test]
 fn trusted_client_cert() {
+    let _cleanup = CleanupBackup;
+
     let pki = TestPki::new_trusted_client();
     let address: SocketAddr = "127.0.0.1:8081".parse().unwrap();
     let mut server = Server::new_test(&pki, &address).expect("Failed to create server");
