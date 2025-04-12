7c1cfcbc73d17a2f65c42346db221ffebe3122db
Trent Shailer <trent.shailer@gmail.com>
2025-04-13 10:30:47 +1200

    Remove old deploy files

--- build-deploy-client.sh
+++
@@ -1,6 +0,0 @@
-cargo build --locked --release --bin backups-client
-mkdir -p ./live/client/
-chmod u+rwx ./target/release/backups-client
-mv ./target/release/backups-client ./live/client/backups-client
-cd ./live/client
-nohup ./backups-client &>/dev/null &
--- build-deploy-server.bat
+++
@@ -1,5 +0,0 @@
-cargo build --locked --release --bin backups-server
-mkdir live\server
-move target\release\backups-server.exe live\server\backups-server.exe
-cd live\server
-start backups-server.exe
